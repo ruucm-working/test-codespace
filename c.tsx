5ca2005284505a9841c8bb9940e5003157775b1b
ruucm a <ruucm.a@gmail.com>
2020-09-28 05:49:38 +0000

    add c

---
+++ c.tsx
@@ -0,0 +1,17 @@
+var boxControl;
+export function Box(): Override {
+    boxControl = useAnimation();
+    return {
+        animate: boxControl,
+    };
+}
+
+export function FrameName(props): Override {
+    return {
+        onTap: () => {
+            boxControl.start({
+                scale: 3,
+            });
+        },
+    };
+}
